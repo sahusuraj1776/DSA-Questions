955c1204fbcde7af749b7cc6c095e9670c3c16b7
Suraj Sahu <sahusuraj1776@gmail.com>
2026-03-14 23:35:33 +0530

    Added Heap Questions

---
+++ Heap/LastStoneWeight.java
@@ -0,0 +1,66 @@
+// 1046. Last Stone Weight
+
+// You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+// We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+// If x == y, both stones are destroyed, and
+// If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+// At the end of the game, there is at most one stone left.
+
+// Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+// Example 1:
+// Input: stones = [2,7,4,1,8,1]
+// Output: 1
+// Explanation: 
+// We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+// we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+// we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+// we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+// Example 2:
+// Input: stones = [1]
+// Output: 1
+ 
+
+// Constraints:
+// 1 <= stones.length <= 30
+// 1 <= stones[i] <= 1000
+
+
+package Heap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LastStoneWeight {
+    public static int lastStoneWeight(int[] stones) {
+        List<Integer> lst = new ArrayList<>();
+        for (int s : stones) lst.add(s);
+        int d1,d2;
+        while(lst.size()>1){
+            Collections.sort(lst);
+            d1 = lst.remove(lst.size()-1);
+            d2 = lst.remove(lst.size()-1);
+            if(d1==d2) continue;
+            lst.add(d1-d2);
+        }
+        return lst.size()==0?0:lst.get(0);
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // for (int s : stones) pq.add(s);
+        // while (pq.size() > 1) {
+        //     int a = pq.poll();
+        //     int b = pq.poll();
+        //     if (a != b) pq.add(a - b);
+        // }
+        // return pq.isEmpty() ? 0 : pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int res = lastStoneWeight(new int[] {2,7,4,1,8,1});
+        System.out.println("Output: "+res);
+    }
+}
